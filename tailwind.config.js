144ba237a9eee0f7ddcfac929a5db89250896b38
ForestParadiseOrigen <andres.jcv.678@gmail.com>
2023-11-20 08:57:57 -0500

    clase_01_angular

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/index.html.{ts,html}",
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
